89727df2f1df769f326b9f9e86635587c5bb2cec
panque-buap <rafael.garciasan@correo.buap.mx>
2025-09-29 18:02:23 -0600

    Se actualiza carpeta Biblioteca y se añade carpeta Lógica

---
+++ Biblioteca/Algoritmos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class AlgoritmosIA():
+    def Algoritmo1IA(self):
+        print("Algoritmo1IA")
+
+class AlgoritmosEstructurasDatos:
+    def AlgoritmoED1(self):
+        print("AlgoritmoED1")
+
+class AlgoritmosColor:
+    def AlgoritmoColor1(self):
+        print("AlgoritmoColor1")
+
+class AlgortimosOdernamiento:
+    def AlgoritmoOrdenamiento1(self):
+        print("AlgoritmoOrdenamiento1")
+
+
+# In[ ]:
+
+
+
+
